c7ee774e76ff80f3ad838b1ceaf5d43e1b0414f2
gabriel conejo <gacova95@gmail.com>
2022-11-28 15:13:06 -0600

    function query de comunidad privada

---
+++ BACKEND/REST_API/src/Funtion_queries/Funtion_queries_comunidad_privada.js
@@ -0,0 +1,57 @@
+const pool = require("../../database");
+const queries = require('../Queries/queries_comunidad_privada');
+const cadenaAleatoria = require("../../extra_f");
+
+
+async function get_comunidad_privada(){
+    const response = await pool.query(queries.get);
+    
+    
+    return response.rows
+    
+}
+
+async function getById_comunidad_privada(id){
+    const response = await pool.query(queries.getById, [id]);
+    return response.rows
+    
+}
+
+async function add_comunidad_privada(NombreComunidad,NombreTorneo,Usuario){
+//cambiar logica
+    const COD_Invita=cadenaAleatoria.cadenaAleatoria();
+    try {
+    const response = await pool.query(queries.add, [NombreComunidad,COD_Invita,NombreTorneo] );}
+    catch {
+        return (401)
+    }
+    return ( 200) 
+}
+
+async function remove_comunidad_privada(id){
+
+    try {
+    const response = await pool.query(queries.remove, [id] );}
+    catch {
+        return (401)
+    }
+    return ( 200) 
+}
+
+async function update_comunidad_privada(id,NombreComunidad,COD_Invita,NombreTorneo){
+
+    try {
+    const response = await pool.query(queries.update, [id, NombreComunidad,COD_Invita,NombreTorneo,id] );}
+    catch {
+        return (401)
+    }
+    return ( 200) 
+}
+
+module.exports = {
+    get_comunidad_privada ,
+    add_comunidad_privada,
+    getById_comunidad_privada,
+    remove_comunidad_privada,
+    update_comunidad_privada,
+} 
